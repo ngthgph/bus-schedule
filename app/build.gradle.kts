4131328fcdc8aa8221ca5549e6c10757abc0ce58
ngthgph <ngthgph112@gmail.com>
2023-12-19 20:58:36 +0700

    Edit gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 
 android {
     namespace = "com.example.busschedule"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.busschedule"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
